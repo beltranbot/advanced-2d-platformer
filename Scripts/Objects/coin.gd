20506acee6563f89543e2b7c8b82f5c9834037f6
beltranbot <beltranbot@gmail.com>
2025-03-18 21:35:17 -0500

    added audio to coins

--- Scripts/Objects/coin.gd
+++ Scripts/Objects/coin.gd
@@ -1,6 +1,15 @@
 extends Area2D
 
 
+@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		queue_free()
+		sprite.hide()
+		audio.play()
+
+
+func _on_audio_stream_player_2d_finished() -> void:
+	queue_free()
