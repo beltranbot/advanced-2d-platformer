f21659b1689e43d34d9fca65ed9ec2918da9baaf
beltranbot <beltranbot@gmail.com>
2025-03-18 21:45:00 -0500

    added game global script

---
+++ Scripts/General/game.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+var health: int = 4
+var max_health: int = 4
+var gold: int = 0
+
--- Scripts/Objects/coin.gd
+++ Scripts/Objects/coin.gd
@@ -4,11 +4,14 @@ extends Area2D
 @onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var coin_value: int = 1
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		sprite.hide()
 		audio.play()
+		Game.gold += coin_value
 
 
 func _on_audio_stream_player_2d_finished() -> void:
