657201ff5b0fe7525e385de04c88f6ee9b3e51de
beltranbot <beltranbot@gmail.com>
2025-03-18 22:16:04 -0500

    added wallcheck to alien

--- Scripts/Monsters/alien.gd
+++ Scripts/Monsters/alien.gd
@@ -7,7 +7,8 @@ const JUMP_VELOCITY = -400.0
 var change_direction: bool = true
 
 
-@onready var ray_wallcliff: RayCast2D = $CliffCheck
+@onready var ray_cliff: RayCast2D = $CliffCheck
+@onready var ray_wall: RayCast2D = $WallCheck
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
@@ -16,15 +17,34 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if not ray_wallcliff.is_colliding():
-		change_direction = !change_direction
-		ray_wallcliff.position.x *= -1
+	_cliff_check()
+	_wall_check()
+	_handle_change_direction()
 
+	move_and_slide()
+
+
+func _cliff_check():
+	if not ray_cliff.is_colliding():
+		_toggle_change_direction()
+		ray_cliff.position.x *= -1
+
+
+func _wall_check():
+	if ray_wall.is_colliding():
+		_toggle_change_direction()
+		ray_wall.position.x *= -1
+		ray_wall.rotation *= -1
+
+
+func _handle_change_direction():
 	if change_direction:
-		velocity.x = -SPEED
+		velocity.x = - SPEED
 		sprite.flip_h = false
 	else:
 		velocity.x = SPEED
 		sprite.flip_h = true
 
-	move_and_slide()
+
+func _toggle_change_direction():
+	change_direction = not change_direction
