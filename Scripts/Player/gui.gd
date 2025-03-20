4dddbf03fcf9986e5bf56eebad15e05bd65d812b
beltranbot <beltranbot@gmail.com>
2025-03-19 23:44:20 -0500

    added GUI scene

---
+++ Scripts/Player/gui.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+
+@onready var gold_label: Label = $Gold
+@onready var heart_container: HBoxContainer = $HeartContainer
+@onready var heart_scene: PackedScene = preload("res://Scenes/Player/heart.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in range(Game.health):
+		var heart: Node = heart_scene.instantiate()
+		heart_container.add_child(heart)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	gold_label.text = "Gold: " + str(Game.gold)
+
+	if Game.health >= 1:
+		if heart_container.get_child_count() > Game.health:
+			heart_container.get_child(heart_container.get_child_count() - 1).queue_free()
+	else:
+		# go to game over
+		pass
